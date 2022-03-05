2fa08c5087a1e8030f60a2fcdfebb07007c0e65e
Thomas <thonoscoding@gmail.com>
2022-03-05 10:22:31 -0500

    f

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+/* Replit.nix
+{ pkgs }: {
+	deps = [
+		pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.yarn
+        pkgs.replitPackages.jest
+	];
+}
+*/
