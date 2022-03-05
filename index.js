edcc7727b5c7341c264f1b37524ac3596ccc6d2a
Thomas Zinno <84212123+TheThonos@users.noreply.github.com>
2022-03-05 15:24:18 +0000

    commit

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 console.log("Hello World!");
-
-/* Replit.nix
-{ pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
-	];
-}
-*/
